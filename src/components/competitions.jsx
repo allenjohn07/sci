7546f628d90ae48fe36dc91ca5c9e0c01fdb8d41
Allen John <allenjohnmonapallil@gmail.com>
2024-08-26 20:33:17 +0530

    competition carousel

--- src/components/competitions.jsx
+++ src/components/competitions.jsx
@@ -3,19 +3,53 @@ import CardComponent from './card';
 import comp1 from "../assets/sci-kumaraguru.jpg"
 import comp2 from "../assets/sci-tamilnadu.jpg"
 import comp3 from "../assets/sci-selam.jpg"
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
 
-const Competitions = ({title}) => {
+const Competitions = ({ title }) => {
+
+    const competitionsArray = [
+        {
+            compImg: comp1,
+            name: "Kumaraguru Open 2024"
+        },
+        {
+            compImg: comp2,
+            name: "TamilNadu Cube Challenge 2024"
+        },
+        {
+            compImg: comp3,
+            name: "Selam Summer Open 2024"
+        }
+    ]
+
+
+
+
     return (
-        <>
+        <div className='space-y-5'>
             <h4 className="scroll-m-20 text-xl font-semibold tracking-tight border-b pb-2">
                 {title}
             </h4>
-            <div className='grid grid-cols-2 md:grid-cols-3 gap-4'>
-                <CardComponent compImg={comp1} name={"Kumaraguru Open 2024"} />
-                <CardComponent compImg={comp2} name={"TamilNadu Cube Challenge 2024"} />
-                <CardComponent compImg={comp3} name={"Selam Summer Open 2024"} />
-            </div>
-        </>
+            <Carousel>
+                <CarouselContent>
+                    {
+                        competitionsArray.map((competition) => (
+                            <CarouselItem key={competition.name} className="basis-1/2 md:basis-1/3">
+                                <CardComponent compImg={competition.compImg} name={competition.name} />
+                            </CarouselItem>
+                        ))
+                    }
+                </CarouselContent>
+                <CarouselPrevious className="hidden md:flex" />
+                <CarouselNext className="hidden md:flex" />
+            </Carousel>
+        </div>
     )
 }
 
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -12,15 +12,17 @@ const HomeComponent = () => {
     },
   };
   return (
-    <div className="container max-w-screen-lg mx-auto px-4 md:px-8 py-8 md:py-14 space-y-10">
+    <div className="container max-w-screen-lg mx-auto px-4 md:px-8 py-8 md:py-14 space-y-5 md:space-y-10">
       <h2 className="scroll-m-20 border-b pb-2 text-3xl text-center font-semibold tracking-tight first:mt-0">
         SpeedCubers India <br />
         <span className='scroll-m-20 mb-10 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0'>WCA Regional Organisation</span>
 
       </h2>
-      <YouTube opts={opts} videoId='yGpSOCBbPvk' />
-      <Competitions title={"Upcoming Competitions"} />
-      <Competitions title={"Past Competitions"} />
+      <div className='pb-5'><YouTube opts={opts} videoId='yGpSOCBbPvk' /></div>
+      <div className='space-y-10 pb-5'>
+        <Competitions title={"Upcoming Competitions"} />
+        <Competitions title={"Past Competitions"} />
+      </div>
       <MoreLinks/>
     </div>
   )
