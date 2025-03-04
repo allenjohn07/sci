1537484b40c8c512029089db33f28d712d240b71
Allen John <allenjohnmonapallil@gmail.com>
2025-03-04 21:43:37 +0530

    Coming Soon

---
+++ src/components/comingSoon.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ComingSoon = () => {
+  return (
+    <div className="flex items-center w-full justify-center h-fit">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">Coming Soon</h1>
+        <p className="text-gray-600">We're working hard to bring you something amazing. Stay tuned!</p>
+      </div>
+    </div>
+  )
+}
+
+export default ComingSoon
